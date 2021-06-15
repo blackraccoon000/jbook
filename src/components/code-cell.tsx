9e4985c8162f8fce1322bb31967fefad8872fa6f
Yutaka Fujii <yutakaf@playwell.site>
2021-06-15 10:22:46 +0900

    editor setting

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,43 +1,44 @@
-import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import { useState, useEffect } from 'react';
 import CodeEditor from './code-editor';
-import Preview from "./preview"
-import bundle from "../bundler"
+import Preview from './preview';
+import bundle from '../bundler';
 import Resizable from './resizable';
 
 const CodeCell = () => {
   const [input, setInput] = useState('');
-  const [error, setError] = useState("")
+  const [error, setError] = useState('');
   const [code, setCode] = useState('');
 
   useEffect(() => {
-    const timer = setTimeout( async () => {
-      const output = await bundle(input)
-      setCode(output.code)
-      setError(output.error)
+    const timer = setTimeout(async () => {
+      const output = await bundle(input);
+      setCode(output.code);
+      setError(output.error);
     }, 500);
     return () => {
-      clearTimeout(timer)
-    }
-  },[input])
+      clearTimeout(timer);
+    };
+  }, [input]);
 
   return (
     <Resizable direction="vertical">
-      <div style={{
-        height: "100%",
-        display: "flex",
-        flexDirection: "row",
-      }}>
+      <div
+        style={{
+          height: '100%',
+          display: 'flex',
+          flexDirection: 'row',
+        }}
+      >
         <Resizable direction="horizontal">
           <CodeEditor
             initialValue="const a = 1;"
             onChange={(value) => setInput(value)}
           />
         </Resizable>
-        <Preview code={code} bundlingStatus={error}/>
+        <Preview code={code} bundlingStatus={error} />
       </div>
     </Resizable>
   );
 };
 
-export default CodeCell
+export default CodeCell;
--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,7 +1,46 @@
-import MDEditor from "@uiw/react-md-editor";
+import './text-editor.css';
+import { useState } from 'react';
+import MDEditor from '@uiw/react-md-editor';
+import { useEffect, useRef } from 'react';
 
 const TextEditor: React.FC = () => {
-  return <div/>
-}
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [editing, setEditing] = useState(false);
+  const [value, setValue] = useState('# Header');
+  // const [editing, setEditing] = useState(true);
 
-export default TextEditor
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (
+        ref.current &&
+        event.target &&
+        ref.current.contains(event.target as Node)
+      ) {
+        return;
+      }
+      setEditing(false);
+    };
+    document.addEventListener('click', listener, { capture: true });
+    return () => {
+      document.removeEventListener('click', listener, { capture: true });
+    };
+  }, []);
+
+  if (editing) {
+    return (
+      <div ref={ref} className="text-editor">
+        <MDEditor value={value} onChange={(v) => setValue(v || '')} />
+      </div>
+    );
+  }
+
+  return (
+    <div onClick={() => setEditing(true)} className="text-editor card">
+      <div className="card-container">
+        <MDEditor.Markdown source={value} />
+      </div>
+    </div>
+  );
+};
+
+export default TextEditor;
